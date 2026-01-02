23845db6b933c6483cf0301a9ee953d941e9ec62
Arjen <newlifearjen@gmail.com>
2026-01-03 01:56:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 /* -------------------------------------------------------------------------- */
 export const metadata: Metadata = {
   title: "Merky Radio",
-  description: "Created by Arjen.",
+  description: "Merky Art Hive Radio | Created by Arjen",
 };
 
 /* -------------------------------------------------------------------------- */
